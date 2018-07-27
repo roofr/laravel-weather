940fc0e472c8508aeab348798be7aa45e2d15ab8
Kevin Redman <iredmedia@gmail.com>
2018-07-26 17:02:53 -0700

    Add comments for ServiceProvider

--- src/WeatherServiceProvider.php
+++ src/WeatherServiceProvider.php
@@ -11,13 +11,16 @@ class WeatherServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Tells Laravel where to look for views when we use laravel-weather::folder.view
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-weather');
 
+        // If we are booting from the console (IE. Artisan)
         if ($this->app->runningInConsole()) {
+            // Allow vendor:publish of config by selecting the package config file, and the path to the apps config
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('weather.php'),
             ], 'config');
-
+            // Allow vendor:publish of views by selecting the package view path, and the path to the deploy to
             $this->publishes([
                 __DIR__.'/../resources/views' => base_path('resources/views/vendor/laravel-weather'),
             ], 'views');
@@ -29,8 +32,10 @@ class WeatherServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Merge the default config from this package with this packages config (laravel-weather::config)
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-weather');
 
+        // Define a singleton for the weather app (ie. app('weather'))
         $this->app->singleton('weather', function ($app) {
             $config = $app->config->get('laravel-weather', array());
 
